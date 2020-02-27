6ccc8965b217e802a34553f28d78fe471e00681b
Alisson Franclin <34225264+alissonf216@users.noreply.github.com>
2020-02-27 10:50:02 -0300

    Update simulacao-monte-carlo.py

--- simulacao-monte-carlo.py
+++ simulacao-monte-carlo.py
@@ -3,9 +3,6 @@ import math
 import pandas_datareader as web
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-from keras.models import Sequential
-from keras.layers import Dense, LSTM
 import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
 
